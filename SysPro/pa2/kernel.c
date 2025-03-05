b56343e5bc8f35f3a6e25af8c2d9263971dd5983
nikitha <109825548+niki-cherukupalli@users.noreply.github.com>
2025-03-06 05:19:48 +0530

    Create kernel.c

---
+++ SysPro/pa2/kernel.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void convert_to_YCrCb(unsigned char *rgb_pixels, unsigned char *ycc_pixels, int width, int height){
+    
+}
+
+int main(int argc, char** argv){
+
+
+}
